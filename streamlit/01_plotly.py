508d805e2edfd56d35dd710fc0ac18716e8d1f5d
SyedAbdulQadirGilani001 <17pwcse1590@uetpeshawar.edu.pk>
2023-02-11 10:41:10 +0500

    updated app

--- streamlit/01_plotly.py
+++ streamlit/01_plotly.py
@@ -1,52 +1,74 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
-
-# Web App Title
-st.markdown('''
-# **The EDA App**
-This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
-**Credit:** App built in `Python` + `Streamlit` 
----
-''')
-
-# Upload CSV data
-with st.sidebar.header('1. Upload your CSV data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file](https://github.com/timothypesi/Data-Sets-For-Machine-Learning-/blob/main/weather-weka.csv)
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+st.write("""
+# Explore different classifier
+Which one is the best?
 """)
+dataset_name=st.sidebar.selectbox('Select Dataset',('Iris','Breast Cancer','Wine'))
+classifier_name=st.sidebar.selectbox('Select Classifier',('KNN','SVM','Random Forest'))
+def get_dataset(dataset_name):
+    if dataset_name=='Iris':
+        data=datasets.load_iris()
+    elif dataset_name=='Breast Cancer':
+        data=datasets.load_breast_cancer()
+    else:
+        data=datasets.load_wine()
+    X=data.data
+    y=data.target
+    return X,y
+X,y=get_dataset(dataset_name) 
+st.write('Shape of dataset:',X.shape)
+st.write('Number of classes:',len(np.unique(y)))
+def add_parameter_ui(clf_name):
+    params=dict()
+    if clf_name=='SVM':
+        C=st.sidebar.slider('C',0.01,10.0)
+        params['C']=C
+    elif clf_name=='KNN':
+        K=st.sidebar.slider('K',1,15)
+        params['K']=K
+    else:
+        max_depth=st.sidebar.slider('max_depth',2,15)
+        n_estimators=st.sidebar.slider('n_estimators',1,100)
+        params['max_depth']=max_depth
+        params['n_estimators']=n_estimators
+    return params
+params=add_parameter_ui(classifier_name)
+def get_classifier(clf_name,params):
+    if clf_name=='SVM':
+        clf=SVC(C=params['C'])
+    elif clf_name=='KNN':
+        clf=KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf=RandomForestClassifier(n_estimators=params['n_estimators'],max_depth=params['max_depth'],random_state=1234)
+    return clf
+clf=get_classifier(classifier_name,params)
+# Classification
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
+clf.fit(X_train,y_train)
+y_pred=clf.predict(X_test)
+acc=accuracy_score(y_test,y_pred)
+st.write(f'Classifier={classifier_name}')
+st.write(f'Accuracy={acc}')
+# Plot
+pca=PCA(2)
+X_projected=pca.fit_transform(X)
+x1=X_projected[:,0]
+x2=X_projected[:,1]
+fig=plt.figure()
+plt.scatter(x1,x2,c=y,alpha=0.8,cmap='viridis')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+# plt.show()
+st.pyplot(fig)
 
-# Pandas Profiling Report
-if uploaded_file is not None:
-    @st.cache_data
-    def load_csv():
-        csv = pd.read_csv(uploaded_file)
-        return csv
-    df = load_csv()
-    pr = ProfileReport(df, explorative=True)
-    st.header('**Input DataFrame**')
-    st.write(df)
-    st.write('---')
-    st.header('**Pandas Profiling Report**')
-    st_profile_report(pr)
-else:
-    st.info('Awaiting for CSV file to be uploaded.')
-    if st.button('Press to use Example Dataset'):
-        # Example data
-        @st.cache_data
-        def load_data():
-            a = pd.DataFrame(
-                np.random.rand(100, 5),
-                columns=['a', 'b', 'c', 'd', 'e']
-            )
-            return a
-        df = load_data()
-        pr = ProfileReport(df, explorative=True)
-        st.header('**Input DataFrame**')
-        st.write(df)
-        st.write('---')
-        st.header('**Pandas Profiling Report**')
-        st_profile_report(pr)
+ 
