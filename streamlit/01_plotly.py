f40c63ed01c2ab9cbbac7bca8f4161753afc3ce8
SyedAbdulQadirGilani001 <17pwcse1590@uetpeshawar.edu.pk>
2023-02-11 12:35:31 +0500

    updated file

--- streamlit/01_plotly.py
+++ streamlit/01_plotly.py
@@ -1,16 +1,9 @@
 import streamlit as st
 from PIL import Image
-st.write("""
-# Add Media to Streamlit
-""")
-st.write("## Image")
-image = Image.open('science.aas9893.fp.png')
-st.image(image, caption='The Snow Leopard', use_column_width=True)
-st.write("## Video")
-video_file = open('Snow Leopards 101 _ Nat Geo Wild.mp4', 'rb')
-video_bytes = video_file.read()
-st.video(video_bytes)
-st.write("## Audio")
-audio_file = open('1605676949_8s685x685x5x4x4ktm65x645685x6ktm65x6456.mp3', 'rb')
-audio_bytes = audio_file.read()
-st.audio(audio_bytes, format='audio/mp3')
+img = Image.open("st.png")
+st.image(img)
+video1 = open("Snow Leopards.mp4", "rb") 
+st.video(video1)
+#st.video(video1, start_time = 25)
+audio1 = open("audio.mp3", "rb")
+st.audio(audio1)
